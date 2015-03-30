bde1a7d62ffb98d531c8feaa9f20483d037358d1
Ronald Gouldner <gouldner@hawaii.edu>
2015-03-30 09:33:05 -1000

    ... fixed run shell for database conversion

--- coeus-db/coeus-db-data-conv/runDataConv.sh
+++ coeus-db/coeus-db-data-conv/runDataConv.sh
@@ -45,6 +45,10 @@ fi
 
 TIME_STAMP=`date +%y.%m.%d.%H.%M.log`
 
-echo "${HOME}"
-
-java -Djava.util.logging.config.file="${HOME}/github/UhKc/coeus-db/coeus-db-data-conv/src/main/resources/org/kuali/coeus/dc/jul-default.properties"   -cp "target/coeus-db-data-conv-6.0.1.jar" org.kuali.coeus.dc.Main ${dryrun} -dbplatform Oracle -dbricecon jdbc:oracle:thin:@localhost:1530:KCDEV -dbcoeuscon jdbc:oracle:thin:@localhost:1530:KCDEV -dbriceuser ${userid} -dbricepwd ${password}  -dbcoeususer ${userid} -dbcoeuspwd ${password} | tee "${0}.${TIME_STAMP}"
+if test -f ./ojdbc6-11.2.0.3.jar
+then
+    java -Djava.util.logging.config.file="${HOME}/github/UhKc/coeus-db/coeus-db-data-conv/src/main/resources/org/kuali/coeus/dc/jul-default.properties"   -cp "target/coeus-db-data-conv-6.0.1.jar;./ojdbc6-11.2.0.3.jar" org.kuali.coeus.dc.Main ${dryrun} -dbplatform Oracle -dbricecon jdbc:oracle:thin:@localhost:1530:KCDEV -dbcoeuscon jdbc:oracle:thin:@localhost:1530:KCDEV -dbriceuser ${userid} -dbricepwd ${password}  -dbcoeususer ${userid} -dbcoeuspwd ${password} | tee "${0}.${TIME_STAMP}"
+else
+    echo "Didn't find oracle jar.  Please copy ojdbc6-11.2.0.3.jar into current directory"
+    exit
+fi
