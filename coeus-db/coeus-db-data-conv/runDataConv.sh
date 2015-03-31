08542bd6d8baaef9fce3960493be764e45fde2fd
Ronald Gouldner <gouldner@hawaii.edu>
2015-03-31 09:49:43 -1000

    data conversion run script...added dryrun option, improved logging and runs both proposal and pprole modes

--- coeus-db/coeus-db-data-conv/runDataConv.sh
+++ coeus-db/coeus-db-data-conv/runDataConv.sh
@@ -1,8 +1,5 @@
 #TODO
 # add options for DB_PORT and TNS_NAME
-# add an argument to remove the dryrun command later when this script is tested
-
-dryrun="-dryrun"
 
 usage()
 {
@@ -12,16 +9,19 @@ usage: $0 -u {db_userid} -p {db_password}
 Run the data conversion process.
 
 OPTIONS:
+        -d dryrun
         -h show this message
         -u db_userid
         -p db_password
 HERE
 }
 
-while getopts "u:p:h" OPTION
+while getopts "u:p:hd" OPTION
 do
         case $OPTION in
                 h) usage
+		   echo "=====================  Displaying db convert usage now (edit run scipt to use any new arguments) =============="
+                   java -Djava.util.logging.config.file="${HOME}/github/UhKc/coeus-db/coeus-db-data-conv/src/main/resources/org/kuali/coeus/dc/jul-default.properties"   -cp "target/coeus-db-data-conv-6.0.1.jar;./ojdbc6-11.2.0.3.jar" org.kuali.coeus.dc.Main -help
                    exit 1
                    ;;
                 u)
@@ -30,6 +30,9 @@ do
                 p)
                    password=$OPTARG
                    ;;
+		d) 
+		   dryrun="-dryrun"
+		   ;;
                 ?)
                    usage
                    exit
@@ -43,11 +46,14 @@ then
     exit 1
 fi
 
-TIME_STAMP=`date +%y.%m.%d.%H.%M.log`
+TIME_STAMP=`date +%y.%m.%d.%H.%M`
 
 if test -f ./ojdbc6-11.2.0.3.jar
 then
-    java -Djava.util.logging.config.file="${HOME}/github/UhKc/coeus-db/coeus-db-data-conv/src/main/resources/org/kuali/coeus/dc/jul-default.properties"   -cp "target/coeus-db-data-conv-6.0.1.jar;./ojdbc6-11.2.0.3.jar" org.kuali.coeus.dc.Main ${dryrun} -dbplatform Oracle -dbricecon jdbc:oracle:thin:@localhost:1530:KCDEV -dbcoeuscon jdbc:oracle:thin:@localhost:1530:KCDEV -dbriceuser ${userid} -dbricepwd ${password}  -dbcoeususer ${userid} -dbcoeuspwd ${password} | tee "${0}.${TIME_STAMP}"
+    echo Running proposal Conversion
+    java -Djava.util.logging.config.file="${HOME}/github/UhKc/coeus-db/coeus-db-data-conv/src/main/resources/org/kuali/coeus/dc/jul-debug.properties"   -cp "target/coeus-db-data-conv-6.0.1.jar;./ojdbc6-11.2.0.3.jar" org.kuali.coeus.dc.Main ${dryrun} -dbplatform Oracle -dbricecon jdbc:oracle:thin:@localhost:1530:KCDEV -dbcoeuscon jdbc:oracle:thin:@localhost:1530:KCDEV -dbriceuser ${userid} -dbricepwd ${password}  -dbcoeususer ${userid} -dbcoeuspwd ${password} proposal 2>&1 | tee "LOGS/${userid}${dryrun}${TIME_STAMP}.ProposalConvertRun.log"
+    echo Running pprole Conversion
+    java -Djava.util.logging.config.file="${HOME}/github/UhKc/coeus-db/coeus-db-data-conv/src/main/resources/org/kuali/coeus/dc/jul-debug.properties"   -cp "target/coeus-db-data-conv-6.0.1.jar;./ojdbc6-11.2.0.3.jar" org.kuali.coeus.dc.Main ${dryrun} -dbplatform Oracle -dbricecon jdbc:oracle:thin:@localhost:1530:KCDEV -dbcoeuscon jdbc:oracle:thin:@localhost:1530:KCDEV -dbriceuser ${userid} -dbricepwd ${password}  -dbcoeususer ${userid} -dbcoeuspwd ${password} pprole 2>&1 | tee "LOGS/${userid}${dryrun}${TIME_STAMP}.PpRoleConvertRun.log"
 else
     echo "Didn't find oracle jar.  Please copy ojdbc6-11.2.0.3.jar into current directory"
     exit
