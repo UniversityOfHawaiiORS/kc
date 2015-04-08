36646ad37fb82a91b8e6feea7daef6387d528d82
Ronald Gouldner <gouldner@hawaii.edu>
2015-04-08 09:59:26 -1000

    Added JPA directives to get UhAwardExtension persistance working

--- coeus-impl/src/main/java/edu/hawaii/award/bo/UhAwardExtension.java
+++ coeus-impl/src/main/java/edu/hawaii/award/bo/UhAwardExtension.java
@@ -2,25 +2,48 @@ package edu.hawaii.award.bo;
 
 import java.sql.Date;
 
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import org.kuali.coeus.common.framework.person.KcPerson;
 import org.kuali.coeus.common.framework.person.KcPersonService;
 import org.kuali.coeus.sys.framework.model.KcPersistableBusinessObjectBase;
 import org.kuali.coeus.sys.framework.service.KcServiceLocator;
 import org.kuali.rice.krad.bo.PersistableBusinessObjectExtension;
 
+@Entity
+@Table(name = "UH_AWARD_EXT_T")
 public class UhAwardExtension extends KcPersistableBusinessObjectBase
 		implements PersistableBusinessObjectExtension {
 
 	private static final long serialVersionUID = 5451523757235950756L;
 
+	@Transient
 	private transient KcPersonService kcPersonService;
-
+	
+	@Id
+    @Column(name = "AWARD_ID")
 	private Long awardId;
-
+	
+	@Column(name = "SIGNED_DATE")
 	private Date signedDate;
+	
+	@Column(name = "ASSIGNED_DATE")
 	private Date assignedDate;
+	
+	@Column(name = "COMPLETED_DATE")
 	private Date completedDate;
+
+	@Column(name = "SPONSOR_ISSUED_DATE")
 	private Date sponsorIssuedDate;
+	
+	@Column(name = "ASSIGNED_TO_PERSON_ID")
 	private String assignedToPersonId;
 
 	public Long getAwardId() {
