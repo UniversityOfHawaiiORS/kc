8d46a1a7624bcb73c5447a44232565bb4a7bbf2c
Ronald Gouldner <gouldner@hawaii.edu>
2015-04-15 16:48:05 -1000

    KC-6.0 Upgrade Commit containing the following JIRA Changes KC-841->KC-841->KC-817->KC-892
    coeus-impl/src/main/resources/edu/hawaii/datadictionary/NegotiationActivity.xml
    
    KC-817:Keep-Spring Bean Overlay These all need to be tested because there is no merge making it difficult to inspect:
    KC-817 Change Negotiation Activity Description to be non-required
    KC-841 Sort Negotiation activity types by function
    
    coeus-impl/src/main/java/edu/hawaii/negotiations/UhNegotiationActivityTypeValuesFinder.java
    
    KC-841:Keep:
    KC-841 Sort Negotiation activity types by function
    
    coeus-impl/src/main/java/org/kuali/kra/negotiations/rules/NegotiationActivityRuleImpl.java
    
    KC-892:merged:
    KC-817 Change Negotiation Activity Description to be non-required
    KC-815 unless activity type other is selected
    KC-892 Enforce some negotiation activity type entries to single occurrence per award

--- coeus-impl/src/main/java/org/kuali/kra/negotiations/rules/NegotiationActivityRuleImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/negotiations/rules/NegotiationActivityRuleImpl.java
@@ -18,14 +18,29 @@
  */
 package org.kuali.kra.negotiations.rules;
 
+import java.util.Calendar;
+// KC-892  Enforce some negotiation activity type entries to single occurrence per award 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+// KC-892  END
+
+
+
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.coeus.sys.framework.service.KcServiceLocator;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.negotiations.bo.Negotiation;
 import org.kuali.kra.negotiations.bo.NegotiationActivity;
+import org.kuali.kra.negotiations.bo.NegotiationActivityType;
 import org.kuali.kra.negotiations.document.NegotiationDocument;
 import org.kuali.coeus.sys.framework.validation.ErrorReporter;
+import org.kuali.rice.coreservice.framework.CoreFrameworkServiceLocator;
+import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.util.GlobalVariables;
+
+import edu.hawaii.infrastructure.UhKeyConstants;
 
 import java.util.Calendar;
 
@@ -79,10 +94,18 @@ public class NegotiationActivityRuleImpl implements NegotiationActivityAddRule {
             result = false;
             errorReporter.reportError(START_DATE_PROPERTY, KeyConstants.ERROR_REQUIRED, "Activity Start Date (Activity Start Date)");
         }
-        if (StringUtils.isBlank(activity.getDescription())) {
+        //  KC-817 Change Negotiation Activity Description to be non-required 
+        //  NOTE: KC-815 unless activity type other is selected
+        String uh_negotiation_description_required_activities = CoreFrameworkServiceLocator
+                .getParameterService().getParameterValueAsString("KC-GEN",
+                                "All", "uh_negotiation_description_required_activities");
+        if (activity.getActivityType() != null 
+        	&& uh_negotiation_description_required_activities.contains("[" + activity.getActivityType().getCode() + "]") 
+        	&& StringUtils.isBlank(activity.getDescription())) {
             result = false;
-            errorReporter.reportError("description", KeyConstants.ERROR_REQUIRED, "Activity Description (Activity Description)");
+            errorReporter.reportError("description", UhKeyConstants.ERROR_NEGOTIATION_DESCRIPTION_REQUIRED, activity.getActivityType().getDescription());
         }
+        //  End KC-815
         if (activity.getStartDate() != null && negotiation.getNegotiationStartDate() != null 
                 && activity.getStartDate().compareTo(negotiation.getNegotiationStartDate()) < 0) {
             result = false;
@@ -110,7 +133,68 @@ public class NegotiationActivityRuleImpl implements NegotiationActivityAddRule {
                 errorReporter.reportError("followupDate", KeyConstants.NEGOTIATION_ACTIVITY_FOLLOWUP_BEFORE_TODAY);
             }
         }
+        // KC-892 Enforce some negotiation activity type entries to single occurrence per award 
+        String violatingActivityTypes = verifyActivityCountRule(activity, negotiation);
+        if(violatingActivityTypes!=null){
+        	result=false;
+           	errorReporter.reportError("activityTypeId", UhKeyConstants.ERROR_NEGOTIATION_SINGLE_ACTIVITY_ALLOWED, violatingActivityTypes);
+        }
+        // KC-892 END
         return result;
     }
 
+    // KC-892 Enforce some negotiation activity type entries to single occurrence per award 
+	private String verifyActivityCountRule(NegotiationActivity activityToVerify, Negotiation negotiation) {
+		List<NegotiationActivity> negotiationActivities = negotiation.getActivities();
+		String violatingActivityTypes=null; 
+		
+		
+		//example of expected format of uh_negotiation_activity_type_required_rules returned by the parameter service :  OR:GR:CR:AR:HE:AA;CG
+		String uh_negotiation_one_entry_allowed_per_activity = CoreFrameworkServiceLocator
+				.getParameterService()
+				.getParameterValueAsString("KC-GEN", "All",	"uh_negotiation_one_entry_allowed_per_activity");
+		
+		String ActivityTypeCodeToVerify=activityToVerify.getActivityType().getCode();
+		String matchingActivityTypeCodeRule=null;
+		//Find the Rules associated with the activity type
+		for (String activityTypeCodeRules : uh_negotiation_one_entry_allowed_per_activity.split(":")) {
+			for (String activityTypeCode : activityTypeCodeRules.split(";")) {
+				// See if rule contains activityType we are checking
+				if (activityTypeCode.equals(ActivityTypeCodeToVerify)) {
+					matchingActivityTypeCodeRule = activityTypeCodeRules;
+				}
+			}
+		}
+				
+		if (matchingActivityTypeCodeRule != null) {
+			boolean violation=false;
+			// Iterate thru the existing activities and check for activities that violate the rules
+			for (NegotiationActivity negotiationActivity : negotiationActivities) {
+				// Check that negotiationActivity is not also the activityToVerify so it doesn't compare to itself and cause error
+				if(activityToVerify!=negotiationActivity) {				    					
+				    for(String activityTypeCodeRule: matchingActivityTypeCodeRule.split(";")){
+				    	if (activityTypeCodeRule.equals(negotiationActivity.getActivityType().getCode())) {
+				    		violation = true;
+					    }
+				    }
+				}
+			}
+			// convert activityTypeCodeRule to a user readable list of activity type code descriptions
+			// Rule may contain one or more activityTypeCodes to be converted to description name(s)
+			if(violation){
+				StringBuilder activityDescriptions = new StringBuilder();
+		    	for( String ruleActivityTypeCode : matchingActivityTypeCodeRule.split(";") ) {
+		            if (activityDescriptions.length() != 0)
+		            	activityDescriptions.append(",");
+		    	    Map<String, String> primaryKeys = new HashMap<String, String>();
+		            primaryKeys.put("NEGOTIATION_ACTIVITY_TYPE_CODE", ruleActivityTypeCode);
+		            NegotiationActivityType activityType = (NegotiationActivityType) KcServiceLocator.getService(BusinessObjectService.class).findByPrimaryKey(NegotiationActivityType.class, primaryKeys);
+		    	    activityDescriptions.append("\"").append(activityType.getDescription()).append("\"");
+		    	}
+		    	violatingActivityTypes=activityDescriptions.toString();
+	    	}
+		}
+		return violatingActivityTypes;
+	}
+	// KC-892 END
 }
