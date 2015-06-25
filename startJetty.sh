270f36a1417f08cef57f3ebd39f21df707ff5ded
Ronald Gouldner <gouldner@hawaii.edu>
2015-06-24 16:43:30 -1000

    changed startJetty.sh to use java 8

--- startJetty.sh
+++ startJetty.sh
@@ -1,6 +1,6 @@
-# Make sure we are running from JAVA 7
-PATH=${PATH//jdk1.6.0_34/jdk1.7.0_51};JAVA_HOME=${JAVA_HOME//jdk1.6.0_34/jdk1.7.0_51};
+# Make sure we are running from JAVA 8 configuration
+PATH=${PATH//jdk1.6.0_34/jdk1.8.0_45};JAVA_HOME=${JAVA_HOME//jdk1.6.0_34/jdk1.8.0_45};PATH=${PATH//jdk1.7.0_51/jdk1.8.0_45};JAVA_HOME=${JAVA_HOME//jdk1.7.0_51/jdk1.8.0_45};java -version;JAVA_PROMPT=Java8;JAVA_OPTS=-Xmx  
 #export JAVA OPTS needed by KC
-export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m -javaagent:C:\Users\ronald\.m2\repository\org\springframework\spring-instrument\3.2.12.RELEASE/spring-instrument-3.2.12.RELEASE.jar"
+export MAVEN_OPTS="-Xmx2048m -javaagent:C:\Users\ronald\.m2\repository\org\springframework\spring-instrument\3.2.12.RELEASE/spring-instrument-3.2.12.RELEASE.jar"
 cd coeus-webapp
 mvn jetty:run
