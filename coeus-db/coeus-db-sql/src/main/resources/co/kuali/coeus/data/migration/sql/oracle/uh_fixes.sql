7b8a053638e45ce370a894ea791f1eb0f656783c
Ronald Gouldner <gouldner@hawaii.edu>
2015-07-15 21:28:23 -1000

    fix issue with release 1506.69 SQL missing from oracle version

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/uh_fixes.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/uh_fixes.sql
@@ -6,4 +6,7 @@ spool uh_fixes.log
 update question set lookup_class = 'org.kuali.coeus.common.framework.custom.arg.ArgValueLookup'
 where lookup_class = 'org.kuali.kra.bo.ArgValueLookup';
 
+insert into KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+values ('KC-GEN','All','JASPER_REPORTS_ENABLED',sys_guid(),1,'CONFG','N','Determines whether the Jasper Report links are available from the KC Application Header. Y to enable, N to disable','A','KC');
+
 commit;
