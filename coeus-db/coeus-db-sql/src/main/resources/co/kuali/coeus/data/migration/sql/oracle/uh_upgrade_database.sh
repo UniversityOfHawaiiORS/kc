e52ceb9f998e272aefd30096d71d0bd951a3cfdc
Ronald Gouldner <gouldner@hawaii.edu>
2015-09-09 19:05:43 -1000

    ... improve upgrade script, ask at every step so I can continue if a step fails by running again

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/uh_upgrade_database.sh
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/uh_upgrade_database.sh
@@ -69,99 +69,101 @@ timestamp=`date +%Y%m%d%H%M`
 LOGDIR=${CURRENT_DIR}/LOGS.${un}.${timestamp}
 mkdir -p ${LOGDIR}
 
-# Update to 5.2.0
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_rice_client_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_kc_upgrade.sql
-
-# Update to 5.2.1
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 521_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 521_oracle_kc_upgrade.sql
-
-# Update to 6.0.0
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_rice_client_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_kc_upgrade.sql
-
-echo "Grepping for errors in the logs"
-mv *.log ${LOGDIR}
-grep -i error ${LOGDIR}/*.log
-
-while [ "${ans}" != "y" ] && [ "${ans}" != "n" ]
+while [ "${ans0}" != "y" ] && [ "${ans0}" != "n" ]
 do
-    read -p "Next Step Run Database Conversion Process Continue ? (y/n)" ans
+    read -p "Run Step 1 - upgrade to 6.0 ? (y/n)" ans0
 done
 
-if [ "${ans}" == "n" ]
+if [ "${ans0}" == "y" ]
 then
-    exit 1
+    # Update to 5.2.0
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_rice_client_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 520_oracle_kc_upgrade.sql
+
+    # Update to 5.2.1
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 521_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 521_oracle_kc_upgrade.sql
+
+    # Update to 6.0.0
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_rice_client_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 600_oracle_kc_upgrade.sql
+
+    echo "Grepping for errors in the logs"
+    mv *.log ${LOGDIR}
+    grep -i error ${LOGDIR}/*.log
 fi
 
-cd ../../../../../../../../../../../coeus-db-data-conv
-./runDataConv.sh -u ${un} -p ${pw} -l ${LOGDIR} -n ${PORT} -s ${DBSvrNm}
+while [ "${ans1}" != "y" ] && [ "${ans1}" != "n" ]
+do
+    read -p "Run Database Conversion Process Part 1 ? (y/n)" ans1
+done
 
-cd ${CURRENT_DIR}
+if [ "${ans1}" == "y" ]
+then
+    cd ../../../../../../../../../../../coeus-db-data-conv
+    ./runDataConv.sh -u ${un} -p ${pw} -l ${LOGDIR} -n ${PORT} -s ${DBSvrNm}
 
-echo "Grepping for errors in the logs"
-grep -i error  ${LOGDIR}/*ConvertRun.log
+    cd ${CURRENT_DIR}
+    echo "Grepping for errors in the logs"
+    grep -i error  ${LOGDIR}/*ConvertRun.log
+fi
+
 
 while [ "${ans2}" != "y" ] && [ "${ans2}" != "n" ]
 do
-    read -p "Next Step Run Post 6.0 upgrade scripts Continue ? (y/n)" ans2
+    read -p "Run Post 6.0 upgrade scripts ? (y/n)" ans2
 done
 
-if [ "${ans2}" == "n" ]
+if [ "${ans2}" == "y" ]
 then
-    exit 1
+    # Update to 6.0.1
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 601_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 601_oracle_kc_upgrade.sql
+
+    # Update to 6.0.2
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 602_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 602_oracle_kc_upgrade.sql
+
+    # Update to 1505
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1505_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1505_oracle_kc_upgrade.sql
+
+    #Update to 1506
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1506_oracle_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1506_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1506_oracle_kc_upgrade.sql
+
+    #Update to 1507 
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1507_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1507_oracle_kc_upgrade.sql
+
+    #Update to 1508
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1508_oracle_kc_rice_server_upgrade.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1508_oracle_kc_upgrade.sql
+
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < uh_fixes.sql
+
+    echo "Done Grepping for errors in the logs"
+    mv *.log ${LOGDIR}
+    grep -i error ${LOGDIR}/*.log
 fi
-
-# Update to 6.0.1
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 601_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 601_oracle_kc_upgrade.sql
-
-# Update to 6.0.2
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 602_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 602_oracle_kc_upgrade.sql
-
-# Update to 1505
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1505_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1505_oracle_kc_upgrade.sql
-
-#Update to 1506
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1506_oracle_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1506_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1506_oracle_kc_upgrade.sql
-
-#Update to 1507 
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1507_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1507_oracle_kc_upgrade.sql
-
-#Update to 1508
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1508_oracle_kc_rice_server_upgrade.sql
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < 1508_oracle_kc_upgrade.sql
-
-sqlplus "${un}"/"${pw}@${DBSvrNm}" < uh_fixes.sql
-
-echo "Done Grepping for errors in the logs"
-mv *.log ${LOGDIR}
-grep -i error ${LOGDIR}/*.log
 
 while [ "${ans3}" != "y" ] && [ "${ans3}" != "n" ]
 do
-    read -p "Next Step Run Database Conversion Process Part 2 Continue ? (y/n)" ans3
+    read -p "Run Database Conversion Process Part 2 ? (y/n)" ans3
 done
 
-if [ "${ans3}" == "n" ]
+if [ "${ans3}" == "y" ]
 then
-    exit 1
+    cd ../../../../../../../../../../../coeus-db-data-conv
+    ./runDataConvPart2.sh -u ${un} -p ${pw} -l ${LOGDIR} -n ${PORT} -s ${DBSvrNm}
+    
+    cd ${CURRENT_DIR}
+
+    echo "Grepping for errors in the logs"
+    grep -i error  ${LOGDIR}/*TimeAndMoney.log
 fi
-
-cd ../../../../../../../../../../../coeus-db-data-conv
-./runDataConvPart2.sh -u ${un} -p ${pw} -l ${LOGDIR} -n ${PORT} -s ${DBSvrNm}
-
-cd ${CURRENT_DIR}
-
-echo "Grepping for errors in the logs"
-grep -i error  ${LOGDIR}/*TimeAndMoney.log
