6ba716ae7ad8b9314c7dfcab9da073f7d282e435
Ronald Gouldner <gouldner@hawaii.edu>
2015-10-23 11:09:02 -1000

    KC-1211 Add Primary Unit for all PIs

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/uh_upgrade_database.sh
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/uh_upgrade_database.sh
@@ -152,6 +152,7 @@ then
     sqlplus "${un}"/"${pw}@${DBSvrNm}" < uh_fixes.sql
     sqlplus "${un}"/"${pw}@${DBSvrNm}" < UH_KRIM_DELTA_PKG.sql
     sqlplus "${un}"/"${pw}@${DBSvrNm}" < COSKeywords.sql
+    sqlplus "${un}"/"${pw}@${DBSvrNm}" < PopulatePrimaryUnitFromProposalCreatorRole.sql
 
     echo "Done Grepping for errors in the logs"
     mv *.log ${LOGDIR}
