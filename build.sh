3f43495128ebadaae3272d764eceb8733a2f8808
Ronald Gouldner <gouldner@hawaii.edu>
2016-02-17 11:11:37 -1000

    modified build so I don't have to unpack, change environment in kc-config-build.xml and rebuild (FOR OTB build)

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-mvn clean install
+# get branch name and set as version
+version=`git name-rev --name-only HEAD`
+# get release year/month for config file folder name (allows me to deploy more than one release)
+release_num=`git name-rev --name-only HEAD | sed s/coeus-// | sed 's/\..*//'`
+
+config_foldername=kc-${release_num}
+
+mvn -Dbuild.version=${version} -Dbuild.config.foldername=${config_foldername} clean install
