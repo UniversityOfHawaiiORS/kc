159bf50f6b79d62accd592907c1e3a772c0484cf
Ronald Gouldner <gouldner@hawaii.edu>
2016-10-05 21:46:37 -1000

    KC-1513 Award COI Project push should initiate on Submit not Save

--- coeus-impl/src/main/java/org/kuali/kra/award/web/struts/action/AwardAction.java
+++ coeus-impl/src/main/java/org/kuali/kra/award/web/struts/action/AwardAction.java
@@ -317,6 +317,15 @@ public class AwardAction extends BudgetParentActionBase {
         ActionForward basicForward = mapping.findForward(KRADConstants.MAPPING_PORTAL);
         ActionRedirect holdingPageForward = new ActionRedirect(mapping.findForward(KcHoldingPageConstants.MAPPING_HOLDING_PAGE));
         holdingPageForward.addParameter(KcHoldingPageConstants.HOLDING_PAGE_DOCUMENT_ID, routeHeaderId);
+
+        // KC-1513 Award COI Project push should initiate on Submit not Save
+        final Project project = getProjectRetrievalService().retrieveProject(awardForm.getAwardDocument().getAward().getAwardNumber());
+
+        if (project != null) {
+            getProjectPublisher().publishProject(project);
+        }
+        // KC-1513 END
+
         return routeToHoldingPage(basicForward, forward, holdingPageForward, returnLocation, routeHeaderId);
     }
     
@@ -440,11 +449,13 @@ public class AwardAction extends BudgetParentActionBase {
          */
         getReportTrackingService().generateReportTrackingAndSave(award, false);
 
+        /* KC-1513 Award COI Project push should initiate on Submit not Save
         final Project project = getProjectRetrievalService().retrieveProject(awardForm.getAwardDocument().getAward().getAwardNumber());
 
         if (project != null) {
             getProjectPublisher().publishProject(project);
         }
+        */
 
         return forward;
     }
