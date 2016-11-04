d62155a7ae6bfa4c6eca6ed1f72e66b8637972cd
Ronald Gouldner <gouldner@hawaii.edu>
2016-11-03 16:43:22 -1000

    KC-1496 ... updated finalized message preferences

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/KC-1496.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/KC-1496.sql
@@ -17,7 +17,7 @@ INSERT INTO KRCR_PARM_T
        (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD,
         VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
 VALUES ('KC-GEN','All','uh_coi_disclosure_revisions_message',sys_guid(),1,'CONFG',
-        'What did we decide for this one ? recalled status',
+        'May need update upon submission',
         'Message to display for disclosure status when PD is in revisions requested status.  HTML links work in the message so links are allowed.  NOTE: if empty or parameter deleted the out of the box responce will be displayed.',
         'A','KC');
 
@@ -25,7 +25,7 @@ INSERT INTO KRCR_PARM_T
        (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD,
         VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
 VALUES ('KC-GEN','All','uh_coi_disclosure_final_message',sys_guid(),1,'CONFG',
-        'Go to COI Dashboard to check status',
+        'Go to your <a href="https://kcdev3.ors.hawaii.edu/coi" target="_blank"> COI Dashboard</a> to check status',
         'Message to display for disclosure status when PD is final.  HTML links work in the message so links are allowed.  NOTE: if empty or parameter deleted the out of the box responce will be displayed.',
         'A','KC');
 
